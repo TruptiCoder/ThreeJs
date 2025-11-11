3f046f3d2b5950b7188f7312e16e39d8b4eb196f
Trupti Balbudhe <truptibalbudhe05@gmail.com>
2025-11-11 18:00:27 +0530

    textures and shaderMaterial

--- Three-Shaders/basic-shader/src/Experience.jsx
+++ Three-Shaders/basic-shader/src/Experience.jsx
@@ -1,12 +1,33 @@
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, useTexture } from "@react-three/drei"
 import vertexShader from './shaders/test/vertex.glsl'
 import fragmentShader from './shaders/test/fragment.glsl'
-import { BufferAttribute, DoubleSide } from "three"
+import { BufferAttribute, DoubleSide, Vector2, Vector3 } from "three"
 import { useEffect, useRef } from "react"
+import { useControls } from 'leva'
+import { useFrame } from "@react-three/fiber"
 
 export default function Experience() {
 
+    const flag = useTexture('./Flag.svg')
+
     const meshRef = useRef()
+    const uniforms = useRef({
+        uFrequency: { value: new Vector2(5, 2) },
+        uTime: { value: 0 },
+        uColor: { value: new Vector3(0.6, 0.2, 1.0) },
+        uTexture: { value: flag }
+    })
+
+    const {wave} = useControls({
+        wave: {
+            value: {x: 5, y: 2},
+            joystick: 'invertY'
+        }
+    })
+
+    useEffect(() => {
+        uniforms.current.uFrequency.value.set(wave.x, wave.y)
+    }, [wave])
 
     useEffect(() => {
         const count = meshRef.current.geometry.attributes.position.count
@@ -17,20 +38,27 @@ export default function Experience() {
         }
 
         meshRef.current.geometry.setAttribute('aRandom', new BufferAttribute(randoms, 1))
+
     }, [])
+
+    useFrame((state, delta) => {
+        const elapsedTime = state.clock.elapsedTime
+        uniforms.current.uTime.value = elapsedTime
+    })
 
   return (
     <>
         <OrbitControls />
         <color args={['#333']} attach={'background'} />
 
-        <mesh ref={meshRef}>
+        <mesh ref={meshRef} scale={[3, 2, 0]}>
             <planeGeometry args={[1, 1, 32, 32]} />
-            <rawShaderMaterial 
+            <shaderMaterial 
                 vertexShader={vertexShader}
                 fragmentShader={fragmentShader}
                 side={DoubleSide}
                 transparent
+                uniforms={uniforms.current}
             />
         </mesh>
 
--- Three-Shaders/basic-shader/src/shaders/test/vertex.glsl
+++ Three-Shaders/basic-shader/src/shaders/test/vertex.glsl
@@ -1,11 +1,11 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+uniform float uTime;
 
-attribute vec3 position;
-attribute float aRandom;
+// attribute float aRandom;
 
 varying float vRandom;
+varying vec2 vUv;
+varying float vElevation;
 
 void main() {
 
@@ -26,13 +26,19 @@ void main() {
     // glsl is similar to c language
 
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    // modelPosition.z += sin(modelPosition.x * 20.0) * 0.1;
-    modelPosition.z += aRandom * 0.1;
+
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+    modelPosition.z += elevation;
+    // modelPosition.z += aRandom * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition;
 
-    vRandom = aRandom;
+    vRandom = modelPosition.y;
+    vUv = uv;
+    vElevation = elevation;
 }
